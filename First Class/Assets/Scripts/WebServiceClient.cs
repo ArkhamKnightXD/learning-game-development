17e22097806cd0dbbb8259d62b97044115cf5669
kevin <karvin_jimenez@hotmail.com>
2020-02-14 01:31:46 -0400

    Cambios finales para realizar la conexion con el servicio web

--- First Class/Assets/Scripts/WebServiceClient.cs
+++ First Class/Assets/Scripts/WebServiceClient.cs
@@ -10,33 +10,29 @@ public class WebServiceClient : MonoBehaviour
     [Serializable]
     public class GravilotaScore
     {
-        public int Id;
-        public string PlayerName;
-        public double Score;
+        public int id;
+        public string playerName;
+        public float score;
     }
+
     UnityWebRequest www;
+
+    const string webServiceURL = "http://localhost:8888/request";
     
     
-    void Start()
-    {
-
-    }
-
-   public IEnumerator SendWebRequest(double score)
+   public IEnumerator SendWebRequest(float score)
     {
        
        GravilotaScore newScore = new GravilotaScore();
 
-       newScore.Id = 0;
-       newScore.PlayerName = "pepe";
-       newScore.Score = score;
-
-       string webServiceURL = $"localhost:8888/save/{newScore.PlayerName}/{newScore.Score}";
+       newScore.id = 20;
+       newScore.playerName = "Karvin";
+       newScore.score = score;
 
        www = UnityWebRequest.Put(webServiceURL, JsonUtility.ToJson(newScore));
        www.SetRequestHeader("Content-Type", "application/json");
        yield return www.SendWebRequest();
 
-        Debug.Log(www.downloadHandler.text);
+       Debug.Log(www.downloadHandler.text);
     }
 }
